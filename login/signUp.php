e4682eadd20a8c60a738d472c4d8482870c35eb0
hanjiwoo <jiw01@naver.com>
2018-10-20 15:06:24 +0900

    modify

--- login/signUp.php
+++ login/signUp.php
@@ -11,15 +11,15 @@ if($pw!=$pwc)
     echo "<a href=signUp.html>back page</a>";
     exit();
 }
-if($id==NULL || $pw==NULL || $name=NULL || $email==NULL)
+if($id==NULL || $pw==NULL || $name==NULL || $nick==NULL)
 {
     echo "칸 비었음";
     echo "<a href=signUp.html>back page</a>";
     exit();
 }
-$mysqli=mysqli_connect("ubuntu","root",2464,"noticeboard");
+$mysql=mysqli_connect("localhost","root",2464,"noticeboard");
 $check="SELECT *from user WHERE user_id='$id'";
-$result=$mysqli->query($check)
+$result=$mysql->query($check)
 
 if($result->num_rows==1){
     echo "중복된 id입니다.";
@@ -27,8 +27,8 @@ if($result->num_rows==1){
     exit();
 }
 
-$signup=mysqli_query(&mysqli,"INSERT INTO user (user_id,user_pw,user_name,user_nick)
-VALUES ('$id','$pw','$name',$nick')");
+$signup=mysqli_query($mysql,"INSERT INTO user (user_id,user_pw,user_name,user_nick)
+VALUES ('$id','$pw','$name','$nick')");
 if($signup){
     echo "회원가입이 정상적으로 완료되었습니다."
 }
